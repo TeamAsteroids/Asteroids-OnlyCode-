c28febd021d4d379fe7f24c57feee226bd5a336c
WaelALR <waelaf95@gmail.com>
2017-12-19 09:13:45 +0100

    Add files via upload

---
+++ Wael/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float maxSpeed = 4.5f;
+    public float rotSpeed = 260f;
+
+
+    // Update is called once per frame
+    void Update () {
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        rot = Quaternion.Euler(0, 0, z);
+        transform.rotation = rot;
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+        pos += rot * velocity;
+        transform.position = pos;
+
+        ;
+    }
+}
