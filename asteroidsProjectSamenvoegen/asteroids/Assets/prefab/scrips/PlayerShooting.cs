bbe5fa1b109f86c4756aa1d286ab007a12cfb3cf
Max Bogaers <d230195@edu.rocwb.nl>
2018-01-11 09:26:09 +0100

    samenvoegen poging 2

---
+++ asteroidsProjectSamenvoegen/asteroids/Assets/prefab/scrips/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    public GameObject bulletPrefab;
+
+    public float fireDelay = 0.25f;
+    public float coolDownTimer = 0;
+
+    private void Update()
+    {
+        coolDownTimer -= Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && coolDownTimer <= 0)
+        {
+            coolDownTimer = fireDelay;
+            Vector3 offset = transform.rotation * bulletOffset;
+            Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+        }else if (Input.GetButton("Fire2") && coolDownTimer <= 0)
+        {
+            coolDownTimer = fireDelay = 0.10f;
+            Vector3 offset = transform.rotation * bulletOffset;
+            Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+        }
+    }
+}
---
+++ asteroidsProjectSamenvoegen/asteroids/Assets/prefab/scrips/ScoreMenuController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreMenuController : MonoBehaviour
+{
+
+    string name = "";
+    public int score;
+    List<Scores> highscore;
+    private bool clicked = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //EventManager._instance._buttonClick += ButtonClicked;
+
+        highscore = new List<Scores>();
+
+    }
+
+
+    void ButtonClicked(GameObject _obj)
+    {
+        print("Clicked button:" + _obj.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Name :");
+        name = GUILayout.TextField(name);
+        GUILayout.EndHorizontal();
+
+
+        if (GUILayout.Button("Add Score") && !clicked )
+        {
+            HighScoreManager._instance.SaveHighScore(name, score);
+            highscore = HighScoreManager._instance.GetHighScore();
+            clicked = true;
+           
+        }
+
+        if (GUILayout.Button("Get LeaderBoard"))
+        {
+            highscore = HighScoreManager._instance.GetHighScore();
+        }
+
+        if (GUILayout.Button("Clear Leaderboard"))
+        {
+            HighScoreManager._instance.ClearLeaderBoard();
+        }
+
+        GUILayout.Space(60);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Name", GUILayout.Width(Screen.width / 2));
+        GUILayout.Label("Scores", GUILayout.Width(Screen.width / 2));
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(25);
+
+        foreach (Scores _score in highscore)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(_score.name, GUILayout.Width(Screen.width / 2));
+            GUILayout.Label("" + _score.score, GUILayout.Width(Screen.width / 2));
+            GUILayout.EndHorizontal();
+        }
+    }
+}
